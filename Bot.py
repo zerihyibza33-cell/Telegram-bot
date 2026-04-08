5b6e33b3cccf7724aacb7101f6a016396a799ec5
zerihyibza33-cell <zerihyibza33@gmail.com>
2026-04-08 13:44:01 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,10 +1,27 @@
-import telebot
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
+from telegram.error import BadRequest
 
 TOKEN = "8772785235:AAFn2OTEAQu_BYKhq-XKMbQBF3EoGpKh2CA"
-bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Hello! I'm your bot 🤖")
+async def hide_join(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("Update received")
 
-bot.infinity_polling()
+    if update.message and update.message.new_chat_members:
+        print("Join detected!")
+        try:
+            await update.message.delete()
+            print("Deleted join message")
+        except BadRequest as e:
+            print("Error:", e)
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, hide_join))
+
+    print("Bot running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
